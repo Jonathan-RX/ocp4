b9128b3beae1747fa27d33a979f2f3944054c359
JonathanRX <jonathan.ravix@gmail.com>
2019-08-17 18:17:14 +0200

    Tests: Modification des tests fonctionels, ajout de diversitée dans le modèle

--- tests/App/Services/CommandTotalTest.php
+++ tests/App/Services/CommandTotalTest.php
@@ -18,35 +18,40 @@ class CommandTotalTest extends TestCase
         $ticket2 = new Tickets();
         $ticket1->setPrice(16);
         $ticket2->setPrice(16);
-        $command->addTicket($ticket1);
-        $command->addTicket($ticket2);
+        $command->addTicket($ticket1)->addTicket($ticket2);
 
         $this->assertSame(32, CommandTotal::getTotalATI($command));
     }
 
-    public function testgetTotalTaxesTwoTickets(){
+    public function testgetTotalTaxesFiveTickets(){
         $command = new Commands();
         $command->setDateCommand(new \DateTime('2030-03-08'))->setDateVisit(New \DateTime('2030-03-08'))->setNbrTickets(2);
         $ticket1 = new Tickets();
         $ticket2 = new Tickets();
-        $ticket1->setPrice(16);
-        $ticket2->setPrice(16);
-        $command->addTicket($ticket1);
-        $command->addTicket($ticket2);
+        $ticket3 = new Tickets();
+        $ticket4 = new Tickets();
+        $ticket5 = new Tickets();
+        $ticket1->setPrice(0);
+        $ticket2->setPrice(8);
+        $ticket3->setPrice(10);
+        $ticket4->setPrice(12);
+        $ticket5->setPrice(16);
+        $command->addTicket($ticket1)->addTicket($ticket2)->addTicket($ticket3)->addTicket($ticket4)->addTicket($ticket5);
 
-        $this->assertSame(5.34, CommandTotal::getTotalTaxes($command));
+        $this->assertSame(7.67, CommandTotal::getTotalTaxes($command));
     }
 
-    public function testgetTotalWTTwoTickets(){
+    public function testgetTotalWTThreeTickets(){
         $command = new Commands();
         $command->setDateCommand(new \DateTime('2030-03-08'))->setDateVisit(New \DateTime('2030-03-08'))->setNbrTickets(2);
         $ticket1 = new Tickets();
         $ticket2 = new Tickets();
-        $ticket1->setPrice(16);
-        $ticket2->setPrice(16);
-        $command->addTicket($ticket1);
-        $command->addTicket($ticket2);
+        $ticket3 = new Tickets();
+        $ticket1->setPrice(8);
+        $ticket2->setPrice(10);
+        $ticket3->setPrice(12);
+        $command->addTicket($ticket1)->addTicket($ticket2)->addTicket($ticket3);
 
-        $this->assertSame(26.66, CommandTotal::getTotalWT($command));
+        $this->assertSame(25.0, CommandTotal::getTotalWT($command));
     }
 }
